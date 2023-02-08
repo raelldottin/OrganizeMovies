1a3259293d2f22308795603369a05a6d5004681c
Raell Dottin <raell.dottin@proton.me>
2023-02-08 08:54:17 -0500

    update the run.py

--- run.py
+++ run.py
@@ -1,55 +1,55 @@
+""" Use to download torrent files from yts.mx website. """
+from typing import NamedTuple
 import argparse
 import log
 import yts
 
 
-'''
-global print_only, download_torrents, log_filename
+class ProgramFlags(NamedTuple):
+    """Program flags"""
 
-print_only = False
-download_torrents = False
-log_filename = ""
-'''
+    print_only: bool
+    download_torrents: bool
+    log_filename: str
 
 
 def main():
-    '''Program entry point'''
-    parser = argparse.ArgumentParser(
-        description='Retrieve torrent files from yts.mx'
-    )
+    """Program entry point"""
+    parser = argparse.ArgumentParser(description="Retrieve torrent files from yts.mx")
     parser.add_argument(
-        '-p',
-        '--print-only',
-        action='store_true',
+        "-p",
+        "--print-only",
+        action="store_true",
         dest="print_only",
         default=False,
-        help='Display torrent file URL only.',
+        help="Display torrent file URL only.",
     )
     parser.add_argument(
-        '-d',
-        '--download-torrents',
-        action='store_true',
-        dest='download_torrents',
+        "-d",
+        "--download-torrents",
+        action="store_true",
+        dest="download_torrents",
         default=False,
-        help='Download torrent file to current directory.',
+        help="Download torrent file to current directory.",
     )
     parser.add_argument(
-        '-l',
-        '--log-file',
+        "-l",
+        "--log-file",
         action="store",
-        dest='log_filename',
+        dest="log_filename",
         default="",
-        help='Log path and file name',
+        help="Log path and file name",
         required=True,
     )
     args = parser.parse_args()
     with log.LogFile(args.log_filename):
-        if args.print_only and args.download_torrents:
-            raise SystemExit('Please only --print_only or --download_torrents.')
+        if args.print_only == args.download_torrents:
+            raise SystemExit("Please only use --print-only or --download-torrents.")
 
-        
-            print_only
-            download_torrents
-            log_filename
-if __name__ == '__main__':
+        flags = ProgramFlags(args.print_only, args.download_torrents, args.log_filename)
+        yts_mx = yts.YTS()
+        yts_mx.run()
+
+
+if __name__ == "__main__":
     main()
