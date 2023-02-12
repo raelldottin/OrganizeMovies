e3d216859227ec9fd3c00f0269fca87a054ea851
Raell Dottin <raell.dottin@proton.me>
2023-02-12 14:20:14 -0500

    change redirect all output to dev null

--- bin/process_directory.sh
+++ bin/process_directory.sh
@@ -1,12 +1,21 @@
 #!/bin/zsh
 
 while true; do
-#  for directory in *YTS.LT* *YTS.MX*; do
-for directory in *YTS.MX* *YTS.LT*; do
-  echo "$directory"
-  if [[ -n "$directory" ]]; then
-    ~/process_torrent.sh "$directory"
+  if ls *YTS.MX* > /dev/null 2>&1; then
+    for directory in *YTS.MX*; do
+      echo "$directory"
+      if [[ -n "$directory" ]]; then
+        ~/process_torrent.sh "$directory"
+      fi
+    done
+    sleep 300
+  elif ls *YTS.LT* > /dev/null 2>&1; then
+    for directory in *YTS.LT*; do
+      echo "$directory"
+      if [[ -n "$directory" ]]; then
+        ~/process_torrent.sh "$directory"
+      fi
+    done
+    sleep 300
   fi
-  done
-  sleep 300
 done
